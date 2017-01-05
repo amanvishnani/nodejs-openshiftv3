906d78ec2ae8b7a33f67946535d805f07e7cbb0c
Aman Vishnani <aman62@gmail.com>
2017-01-06 01:30:35 +0530

    commit

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var app = new express();
 var mongoose = require('mongoose');
 
 app.set('MongoService',process.env.DATABASE_SERVICE_NAME.toUpperCase());
-app.set('MongoHost',process.env[app.get('MongoService') + '_SERVICE_HOST']);
-app.set('MongoPort',parseInt(process.env[app.get('MongoService') + '_SERVICE_PORT']));
-app.set('MongoUser',process.env[app.get('MongoService') + '_USER']);
-app.set('MongoPass',process.env[app.get('MongoService') + '_PASSWORD']);
-app.set('DbName', process.env[app.get('MongoService') + '_DATABASE']);
+app.set('MongoHost',process.env[process.env.DATABASE_SERVICE_NAME + '_SERVICE_HOST']);
+app.set('MongoPort',process.env[process.env.DATABASE_SERVICE_NAME + '_SERVICE_PORT']);
+app.set('MongoUser',process.env[process.env.DATABASE_SERVICE_NAME + '_USER']);
+app.set('MongoPass',process.env[process.env.DATABASE_SERVICE_NAME + '_PASSWORD']);
+app.set('DbName', process.env[process.env.DATABASE_SERVICE_NAME + '_DATABASE']);
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT|| 8080);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP ||process.env.IP|| '0.0.0.0');
 
@@ -23,6 +23,8 @@ var SEND = "Not Connected";
 //     SEND +=mongoURL;
 //     SEND +='<br/> Hello World';
 // });
+SEND +='<br/>';
+SEND +=app.get('MongoService');
 SEND +='<br/>';
 SEND +=mongoURL;
 
